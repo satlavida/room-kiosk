dfc25a2de242e553fac37403241c57a0401d6a8d
Satyajeet Nigade <satyajeetnigade@outlook.com>
2025-05-14 17:13:01 +0530

    Dash board component

--- src/app/features/room-kiosk/room-kiosk.module.ts
+++ src/app/features/room-kiosk/room-kiosk.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { RoomKioskRoutingModule } from './room-kiosk-routing.module';
 import { RoomKioskComponent } from './room-kiosk.component';
+import { DashboardPageComponent } from './pages/dashboard-page/dashboard-page.component';
 
 
 @NgModule({
   declarations: [
-    RoomKioskComponent
+    RoomKioskComponent,
+    DashboardPageComponent
   ],
   imports: [
     CommonModule,
